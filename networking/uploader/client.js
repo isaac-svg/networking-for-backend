168d68ed008059243d4cf2954062d660e515ca3b
isaac-svg <isaacsakyi009@gmail.com>
2023-10-17 13:35:20 +0000

    Done with day four

--- networking/uploader/client.js
+++ networking/uploader/client.js
@@ -1,10 +1,70 @@
 const net = require("net")
-const fs =  require("node:fs/promises")
+const fs =  require("node:fs/promises");
+const path = require("path");
 
+// let fileHanlde, fileStream;
+const clearLine = (direction)=>{
+
+    return new Promise((resolve, reject)=>{
+
+        process.stdout.clearLine(direction,()=>{
+            resolve()
+        })
+    })
+}
+
+
+const moveCursor = (dx, dy) =>{
+    return new Promise((resolve, reject)=>{
+        process.stdout.moveCursor(dx, dy, ()=>{
+            resolve()
+        })
+    })
+}
 
 
 const socket = net.createConnection(
-    {host:"::1", port:5050}
+    {host:"::1", port:5050},
+    async ()=>{
+
+        const filePath =  process.argv[2]
+        const fileName = path.basename(filePath)
+        const fileHanlde = await fs.open(filePath, "r")
+        const fileStream = fileHanlde.createReadStream()
+        const fileSize = (await fileHanlde.stat()).size
+
+        let uploadedPercentage = 0
+        let bytesUploaded = 0
+
+         socket.write(`filename: ${fileName}-------`)
+        // reading from source file
+        fileStream.on("data", async (data)=>{
+            if (!socket.write(data)){
+                fileStream.pause()
+            }
+            bytesUploaded += data.length
+
+            let newPercentage = Math.floor((bytesUploaded / fileSize) * 100)
+
+            if (newPercentage != uploadedPercentage){
+                uploadedPercentage = newPercentage
+               await moveCursor(0, -1)
+               await clearLine(0)
+                console.log(`Uploading...${uploadedPercentage}%`)
+            }
+        })
+
+        socket.on("drain",()=>{
+            fileStream.resume()
+        })
+        fileStream.on("end", ()=>{
+
+            console.log("File successully uploadded!")
+
+            socket.end()
+        })
+
+    }
 )
 
 
--- networking/uploader/server.js
+++ networking/uploader/server.js
@@ -1,19 +1,45 @@
 const net  = require("net")
 const fs =  require("node:fs/promises")
-const server = net.createServer(()=>{
-
-})
+const server = net.createServer(()=>{})
+const path = require("path")
 
 server.on("connection", (socket)=>{
+    let fileHanlde, fileStream;
     console.log("New connection")
 
 
     socket.on("data", async (data)=>{
 
-        const fileHanlde =  await fs.open("/storage/text.txt");
-        const fileStream = fileHanlde.createWriteStream()
-        fileStream.write(data)
+        if (!fileHanlde)
+        {
+            socket.pause() // stop recieving buffer from the client
+
+            const indexOfDivider = data.indexOf("-------")
+            const fileName = data.subarray(10, indexOfDivider)
+
+            fileHanlde =  await fs.open(`storage/${fileName}`, "w");
+            fileStream = fileHanlde.createWriteStream()
+
+           // writing to destination file discard the headers
+           fileStream.write(data.subarray(indexOfDivider + 7))
+
+            socket.resume() // start receiving from the client
+           fileStream.on("drain", ()=>{
+            socket.resume()
+           })
+        }else {socket
+            if (!fileStream.write(data)){
+                socket.pause()
+            }
+        }
         
+    })
+
+    socket.on("end",()=>{
+        fileHanlde.close()
+        console.log("Connection ended")
+        fileHanlde = undefined
+        fileStream = undefined
     })
 
 })
